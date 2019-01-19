cb616ab508d561aebe6ec61b8c36926a74e9720e
Houseeq <fallingsand2608@gmail.com>
2019-01-19 09:18:30 +0800

    new project

--- cmds/add-roles.js
+++ cmds/add-roles.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 module.exports.run = async (bot, message, args) => {
 
     if (args.length === 0){
-        message.channel.send("A role is necessary for the command: !add-role @Moderator");
+        message.channel.send("A role is necessary for the command: !add-roles @Moderator");
     }else if (args.length > 0){
         let roles_raw = fs.readFileSync('./roles.json');
         let roles_array = JSON.parse(roles_raw);
@@ -15,7 +15,7 @@ module.exports.run = async (bot, message, args) => {
         for (var i = 0; i < roles_array.roles.length; i++){
             if (role === roles_array.roles[i]){
                 found = true
-                message.channel.send(role +"Role is already allowed.");
+                message.channel.send(role + "role is already allowed.");
                 return;
             }
         }
@@ -24,7 +24,7 @@ module.exports.run = async (bot, message, args) => {
             roles_array.roles.push(role);
             let roles_write = JSON.stringify(roles_array)
             fs.writeFileSync('./roles.json', roles_write);
-			message.channel.send(role +"Role added successfully.");
+			message.channel.send(role + "role added successfully.");
         }
     } 
 
